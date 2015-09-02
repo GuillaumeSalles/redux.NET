b4f7258d54fe08a6f3abfa637d21e3c076f950ea
Guillaume Salles <guillaume.salles@me.com>
2015-09-01 21:56:41 -0400

    MyGet + nuspec

---
+++ nuget/build.bat
@@ -0,0 +1,22 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+ 
+set version=0.1.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+set nuget=
+if "%nuget%" == "" (
+	set nuget=nuget
+)
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild ..\src\Redux.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+
+mkdir Build
+mkdir Build\lib
+
+%nuget% pack "ReduxNet.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
