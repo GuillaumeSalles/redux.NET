921f31ae1341ce1b8c859f9b0b3eaed0ed774c8c
Guillaume Salles <guillaume.salles@me.com>
2015-09-03 00:32:44 -0400

    Redux.NET.DevTools nuspec

--- nuget/MyGet.bat
+++ nuget/MyGet.bat
@@ -13,4 +13,5 @@ nuget\nuget.exe restore src\Redux.sln
 
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" src\Redux.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
-nuget\nuget.exe pack "nuget\ReduxNet.nuspec" -NoPackageAnalysis -verbosity detailed -Version %version% -p Configuration="%config%"
+nuget\nuget.exe pack "nuget\Redux.NET.nuspec" -NoPackageAnalysis -verbosity detailed -Version %version% -p Configuration="%config%"
+nuget\nuget.exe pack "nuget\Redux.NET.DevTools.nuspec" -NoPackageAnalysis -verbosity detailed -Version %version% -p Configuration="%config%"
