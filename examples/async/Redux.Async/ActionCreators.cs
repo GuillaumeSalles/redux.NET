1d4707ae4dd94f43783e9bd4c1ac27a1817a6ff0
Guillaume Salles <guillaume.salles@me.com>
2015-11-02 23:56:16 -0500

    Add basic http example

---
+++ examples/async/Redux.Async/ActionCreators.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Redux.Async.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Redux.Async
+{
+    public static class ActionCreators
+    {
+        public static async Task<IAction> SearchRepositories(string searchTerm)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("Redux.NET async app");
+
+            var jsonResponse = await client.GetStringAsync(new Uri($"https://api.github.com/search/repositories?q={searchTerm}&sort=stars&order=desc"));
+
+            var searchResponse = JsonConvert.DeserializeObject<SearchRepositoriesResponse>(jsonResponse);
+
+            return new SearchRepositoriesAction
+            {
+                Repositories = searchResponse.Items
+            };
+        }
+    }
+}
