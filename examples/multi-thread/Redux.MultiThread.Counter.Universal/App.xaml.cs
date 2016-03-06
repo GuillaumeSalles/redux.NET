05f7fbb092035b06d30b41f1c27cb28ebd39c8d9
Guillaume Salles <guillaume.salles@me.com>
2016-03-06 01:15:42 -0500

    Add multi thread-sample

---
+++ examples/multi-thread/Redux.MultiThread.Counter.Universal/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Redux.MultiThread.Counter.Universal
+{
+    sealed partial class App : Application
+    {
+        public static IStore<int> Store { get; } = new Store<int>(Reducers.ReduceCounter, 0);
+
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame == null)
+            {
+                rootFrame = new Frame();
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+            }
+
+            Window.Current.Activate();
+        }
+    }
+}
