e21b60c856c57b65b40d8ab84cbd41f2732bd630
Aaron Roberts <aaron.roberts@deluxe.com>
2017-03-28 17:29:39 -0400

    extract delegates, IStore and add simple explanations

---
+++ src/Redux/IStore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Redux 
+{
+    /// <summary>
+    ///     Represents a store that encapsulates a state tree and is used to dispatch actions to update the
+    ///     state tree.
+    /// </summary>
+    /// <typeparam name="TState">
+    ///     The state tree type.
+    /// </typeparam>
+    public interface IStore<TState> : IObservable<TState>
+    {
+        /// <summary>
+        ///     Dispatches an action to the store.
+        /// </summary>
+        /// <param name="action">
+        ///     The action to dispatch.
+        /// </param>
+        /// <returns>
+        ///     Varies depending on store enhancers. With no enhancers Dispatch returns the action that 
+        ///     was passed to it.
+        /// </returns>
+        object Dispatch(object action);
+
+        /// <summary>
+        ///     Gets the current state tree.
+        /// </summary>
+        /// <returns>
+        ///     The current state tree.
+        /// </returns>
+        TState GetState();
+    }
+}
