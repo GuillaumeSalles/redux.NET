419db1dcc6c6a3460ba43ee3a45283146519ef6a
Guillaume Salles <guillaume.salles@me.com>
2017-03-31 17:34:57 -0400

    ObserveState extension

---
+++ src/Redux/Reactive/StoreExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace Redux.Reactive
+{
+    public static class StoreExtensions
+    {
+        public static IObservable<T> ObserveState<T>(this IStore<T> store)
+        {
+            return Observable.FromEvent<T>(
+                h => store.StateChanged += h,
+                h => store.StateChanged -= h);
+        }
+    }
+}
