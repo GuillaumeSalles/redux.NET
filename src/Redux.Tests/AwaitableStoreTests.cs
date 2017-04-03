4f8f53c7f09dd633940373ee70295e12637d35ea
Christer van der Meeren <cmeeren@protonmail.com>
2017-04-03 11:58:49 +0200

    Add action-observable store and awaitable store with awaitable async saga support

---
+++ src/Redux.Tests/AwaitableStoreTests.cs
@@ -0,0 +1,150 @@
+﻿namespace Redux.Tests
+{
+    using System.Reactive.Linq;
+    using System.Threading.Tasks;
+
+    using NUnit.Framework;
+
+    public class AwaitableStoreTests
+    {
+        private class IncrementAction
+        {
+        }
+
+        private class IncrementAsyncAction
+        {
+        }
+
+        private static int Reducer(int state, object action)
+        {
+            switch (action)
+            {
+                case IncrementAction _: return state + 1;
+                default: return state;
+            }
+        }
+
+        private async Task DelayedIncrementSaga(IncrementAsyncAction action, IStore<int> store)
+        {
+            await Task.Delay(1000);
+            store.Dispatch(new IncrementAction());
+        }
+
+        private void ImmediateIncrementSaga(IncrementAsyncAction action, IStore<int> store)
+        {
+            store.Dispatch(new IncrementAction());
+        }
+
+        [Test]
+        public async Task When_AwaitingDispatch_Should_GetUpdatedState()
+        {
+            // Arrange
+            var awaitableStore = new AwaitableStore<int>(Reducer, 0);
+            awaitableStore.Actions.OfType<IncrementAsyncAction>().RunsAsyncSaga(awaitableStore, this.DelayedIncrementSaga);
+
+            // Act
+            await awaitableStore.DispatchAsync(new IncrementAsyncAction());
+
+            // Assert
+            Assert.That(awaitableStore.GetState(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void When_NotAwaitingDispatchAsync_Should_GetOldState()
+        {
+            // Arrange
+            var awaitableStore = new AwaitableStore<int>(Reducer, 0);
+            awaitableStore.Actions.OfType<IncrementAsyncAction>().RunsAsyncSaga(awaitableStore, this.DelayedIncrementSaga);
+
+            // Act
+            awaitableStore.DispatchAsync(new IncrementAsyncAction());
+
+            // Assert
+            Assert.That(awaitableStore.GetState(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void When_NormalDispatch_Should_GetOldState()
+        {
+            // Arrange
+            var awaitableStore = new AwaitableStore<int>(Reducer, 0);
+            awaitableStore.Actions.OfType<IncrementAsyncAction>().RunsAsyncSaga(awaitableStore, this.DelayedIncrementSaga);
+
+            // Act
+            awaitableStore.Dispatch(new IncrementAsyncAction());
+
+            // Assert
+            Assert.That(awaitableStore.GetState(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void When_SagaNotInvoked_Should_GetNewStateWithNonAwaitedDispatchAsync()
+        {
+            // Arrange
+            var awaitableStore = new AwaitableStore<int>(Reducer, 0);
+            awaitableStore.Actions.OfType<IncrementAsyncAction>().RunsAsyncSaga(awaitableStore, this.DelayedIncrementSaga);
+
+            // Act
+            awaitableStore.Dispatch(new IncrementAction());
+
+            // Assert
+            Assert.That(awaitableStore.GetState(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void When_SagaNotInvoked_Should_GetNewStateWithNormalDispatch()
+        {
+            // Arrange
+            var awaitableStore = new AwaitableStore<int>(Reducer, 0);
+            awaitableStore.Actions.OfType<IncrementAsyncAction>().RunsAsyncSaga(awaitableStore, this.DelayedIncrementSaga);
+
+            // Act
+            awaitableStore.Dispatch(new IncrementAction());
+
+            // Assert
+            Assert.That(awaitableStore.GetState(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void When_UsingNonAsyncSaga_Should_GetNewStateWithNormalDispatch()
+        {
+            // Arrange
+            var awaitableStore = new AwaitableStore<int>(Reducer, 0);
+            awaitableStore.Actions.OfType<IncrementAsyncAction>().RunsSaga(awaitableStore, this.ImmediateIncrementSaga);
+
+            // Act
+            awaitableStore.Dispatch(new IncrementAction());
+
+            // Assert
+            Assert.That(awaitableStore.GetState(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void When_UsingNonAsyncSaga_Should_GetNewStateNonAwaitedAsyncDispatch()
+        {
+            // Arrange
+            var awaitableStore = new AwaitableStore<int>(Reducer, 0);
+            awaitableStore.Actions.OfType<IncrementAsyncAction>().RunsSaga(awaitableStore, this.ImmediateIncrementSaga);
+
+            // Act
+            awaitableStore.DispatchAsync(new IncrementAction());
+
+            // Assert
+            Assert.That(awaitableStore.GetState(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public async Task When_UsingNonAsyncSaga_Should_GetNewStateAwaitedAsyncDispatch()
+        {
+            // Arrange
+            var awaitableStore = new AwaitableStore<int>(Reducer, 0);
+            awaitableStore.Actions.OfType<IncrementAsyncAction>().RunsSaga(awaitableStore, this.ImmediateIncrementSaga);
+
+            // Act
+            await awaitableStore.DispatchAsync(new IncrementAction());
+
+            // Assert
+            Assert.That(awaitableStore.GetState(), Is.EqualTo(1));
+        }
+    }
+}
---
+++ src/Redux/AwaitableStore.cs
@@ -0,0 +1,105 @@
+﻿namespace Redux
+{
+    using System;
+    using System.Reactive.Linq;
+    using System.Reactive.Subjects;
+    using System.Threading.Tasks;
+
+    public delegate void Saga<TState, in TAction>(TAction action, IStore<TState> store);
+
+    public delegate Task AsyncSaga<TState, in TAction>(TAction action, IStore<TState> store);
+
+    public static class ObservableExtensions
+    {
+        public static void RunsSaga<TState, TAction>(
+            this IObservable<TAction> source,
+            IStore<TState> store,
+            Saga<TState, TAction> saga)
+        {
+            source.Subscribe(action => saga(action, store));
+        }
+
+        public static void RunsAsyncSaga<TState, TAction>(
+            this IObservable<TAction> source,
+            AwaitableStore<TState> store,
+            AsyncSaga<TState, TAction> saga)
+        {
+            source.Subscribe(
+                async action =>
+                {
+                    // TODO: Find a way to call AddOperation and RemoveOperation below
+                    // without specifying concrete class AwaitableStore above, but also
+                    // without giving devs access to AddOperation and RemoveOperation
+                    store.AddOperation();
+                    await saga(action, (IStore<TState>)store);
+                    store.RemoveOperation();
+                });
+        }
+    }
+
+    public interface IObservableActionStore
+    {
+        IObservable<object> Actions { get; }
+    }
+
+    public class ObservableActionStore<TState> : Store<TState>, IObservableActionStore
+    {
+        private readonly ISubject<object> actionsSubject = new Subject<object>();
+
+        /// <inheritdoc />
+        public ObservableActionStore(
+            Reducer<TState> reducer,
+            TState initialState = default(TState),
+            params Middleware<TState>[] middlewares) : base(reducer, initialState, middlewares)
+        {
+        }
+
+        public IObservable<object> Actions => this.actionsSubject;
+
+        protected override object InnerDispatch(object action)
+        {
+            object ret = base.InnerDispatch(action);
+            this.actionsSubject.OnNext(action);
+            return ret;
+        }
+    }
+
+    public interface IAwaitableStore<TState>
+    {
+
+        Task<object> DispatchAsync(object action);
+    }
+
+    public class AwaitableStore<TState> : ObservableActionStore<TState>, IAwaitableStore<TState>
+    {
+        private int numOperations;
+        private readonly ISubject<int> numOperationsSubject = new BehaviorSubject<int>(0); // TODO: start with 0
+
+        /// <inheritdoc />
+        public AwaitableStore(
+            Reducer<TState> reducer,
+            TState initialState = default(TState),
+            params Middleware<TState>[] middlewares) : base(reducer, initialState, middlewares)
+        {
+        }
+
+        private IObservable<int> OngoingOperations => this.numOperationsSubject;
+
+        internal void AddOperation()
+        {
+            this.numOperationsSubject.OnNext(++this.numOperations);
+        }
+
+        internal void RemoveOperation()
+        {
+            this.numOperationsSubject.OnNext(--this.numOperations);
+        }
+
+        public async Task<object> DispatchAsync(object action)
+        {
+            object ret = this.Dispatch(action);
+            await this.OngoingOperations.FirstAsync(i => i == 0);
+            return Task.FromResult(ret);
+        }
+    }
+}
